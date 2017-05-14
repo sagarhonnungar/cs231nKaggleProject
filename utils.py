49d5fdcb57ae036c98df8b547a10c31220bc8a46
sagarhonnungar <sagar1honnungar@gmail.com>
2017-05-14 09:14:16 +0000

    modified optimise_f2_thresholds

--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
+import numpy as np
 from sklearn.metrics import fbeta_score
 
-def optimise_f2_thresholds(y, p, verbose=True, resolution=100):
+def optimise_f2_thresholds(y, p, num_classes=17, verbose=True, resolution=100):
   def mf(x):
     p2 = np.zeros_like(p)
-    for i in range(17):
+    for i in range(num_classes):
       p2[:, i] = (p[:, i] > x[i]).astype(np.int)
     score = fbeta_score(y, p2, beta=2, average='samples')
     return score
 
-  x = [0.2]*17
-  for i in range(17):
+  x = [0.2]*num_classes
+  for i in range(num_classes):
     best_i2 = 0
     best_score = 0
     for i2 in range(resolution):
